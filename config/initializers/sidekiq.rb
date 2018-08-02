258a26b7145bffa99cbf0f0923b5c10c148f6da8
Artem Kolesnikov <sirruf@me.com>
2018-08-03 01:55:13 +0300

    deploy config updated

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,18 @@ require 'sidekiq'
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
+if ENV['REDIS_URL']
+  $redis = ENV['REDIS_URL']
+else
+  $redis = 'redis://127.0.0.1:6379/0'
+end
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_monitoring_#{Rails.env}" }
+  config.redis = { url: $redis, namespace: "sidekiq_monitoring_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/0', namespace: "sidekiq_monitoring_#{Rails.env}" }
+  config.redis = { url: $redis, namespace: "sidekiq_monitoring_#{Rails.env}" }
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
